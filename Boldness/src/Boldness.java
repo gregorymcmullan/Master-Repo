d95bdc0b74811e00878e5d9c67847b55caa3258d
tanner.amman <tanner.amman@MU-C1-09.mullen.pvt.k12.co.us>
2020-01-09 11:16:10 -0700

    added meetPerson

--- Boldness/src/Boldness.java
+++ Boldness/src/Boldness.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Boldness
 	{
@@ -6,11 +7,19 @@ public class Boldness
 			{
 				System.out.println("Be bold!");
 
-        System.out.println("I agree be bold");
+				System.out.println("I agree be bold");
 
 				System.out.println("No! be BOLDER!!");
-
+				
 
 			}
+		public void meetPerson()
+		{
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("What is your name?");
+			String name = userInput.nextLine();
+			System.out.println("Hello, " + name + "! I hope your day is going swimmingly!");
+			int number = 10;
+		}
 
 	}
